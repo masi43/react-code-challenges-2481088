1355aef55c4ec7589a7b6d401856acf7fb60012a
masi43 <70043480+masi43@users.noreply.github.com>
2022-08-26 20:10:03 -0500

    add dark mode and light mode feature on buttonclick

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,16 @@
+import React, {useState} from 'react'
+
 export default function DarkMode () {
+
+  const [mode, setMode] = useState('light-mode')
+
+  const changeMode = function(modeData) {
+    setMode(modeData === 'dark' ? 'dark-mode' : 'light-mode');
+  }
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className={`page ${mode}`}>
+      <button className='dark-mode-button' onClick={() => changeMode('dark')}>Dark Mode</button>
+      <button className='light-mode-button' onClick={() => changeMode('light')}>Light Mode</button>
     </div>
   )
 }
