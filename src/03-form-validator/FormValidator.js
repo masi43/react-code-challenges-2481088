644d548ee61d5d8c3fa8e24317d797b7d406b5b5
masi43 <70043480+masi43@users.noreply.github.com>
2022-08-26 21:21:16 -0500

    validate and add validation messages to form

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -4,9 +4,35 @@ export default function FormValidator () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
 
+  const getValidationString = function(emailArg, pwdArg, pwdConfrimArg) {
+    if(emailArg.length === 0 || pwdArg.length === 0 || pwdConfrimArg.length === 0) {
+      return 'please enter email/password/password confirmation'
+    } 
+    const emailSymbolArr = emailArg.split('').filter((alphabet) => (alphabet === '@'))
+    if(emailSymbolArr.length === 0 || emailSymbolArr.length > 1) {
+      return 'email should contain one and only one @ symbol'
+    } 
+
+    if(pwdArg.length < 8) {
+      return 'password should be atleast 8 characters long'
+    }
+
+    if(pwdArg !== pwdConfrimArg) {
+      return 'password and password confirmation must be same'
+    }
+
+    return 'success';
+  }
+  const handleSubmit = function(event) {
+    event.preventDefault()
+    
+    setErrorMessage(getValidationString(email, password, passwordConfirm))
+}
   return (
-    <form>
+    <>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
@@ -25,5 +51,7 @@ export default function FormValidator () {
       />
       <input type='submit' value='Submit' />
     </form>
+    <p> {errorMessage} </p>
+    </>
   )
 }
