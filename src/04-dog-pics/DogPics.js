a9067b1baafd248e8082a20b65c0c12378347d7f
masi43 <70043480+masi43@users.noreply.github.com>
2022-08-29 10:07:42 -0500

    add random dog pic useing useEffect, async

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,32 @@
+import {useState, useEffect} from 'react'
+
+const getImageSrc = async (url) => {
+  const resultJson = await fetch(url);
+  const result = await resultJson.json();
+  return result.message;
+}
+
 export default function DogPics () {
   // API: https://dog.ceo/dog-api/
+
+  const [imgSrc, setImgSrc] = useState('')
+
+  useEffect( () => {
+    getImageSrc('https://dog.ceo/api/breeds/image/random').then(pic => setImgSrc(pic));
+  },[])
+
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={imgSrc} />
+      <button 
+        onClick={
+         async () =>  setImgSrc( 
+            await getImageSrc('https://dog.ceo/api/breeds/image/random')
+          )
+        }
+      >
+        🐶
+      </button>
     </div>
   )
 }
