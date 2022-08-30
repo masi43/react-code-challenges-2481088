cfad34fe51ee4f88bf7cced96da658585dc05d6b
masi43 <70043480+masi43@users.noreply.github.com>
2022-08-30 11:02:38 -0500

    add eventlistener in react component on useEffect and remove the listener on unmount

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,4 +1,15 @@
+import {useEffect} from 'react';
+
 export default function WindowEvent () {
+  useEffect( () => {
+    const doubleClick = () => alert("mouse pressed");
+    // callback in add and remove should be same check below link for explanation
+    // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener#matching_event_listeners_for_removal
+    window.addEventListener('dblclick', doubleClick);
+
+    return () =>  window.removeEventListener('dblclick', doubleClick);
+      
+  },[])
   return (
     <h2>Window event active</h2>
   )
