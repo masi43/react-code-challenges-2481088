8b3109d4a2b0c3f605b7db9fe4a0b5c922281f32
masi43 <70043480+masi43@users.noreply.github.com>
2022-09-01 13:54:34 -0500

    add shoopig cart

--- src/11-shopping-cart/ShoppingCart.js
+++ src/11-shopping-cart/ShoppingCart.js
@@ -1,4 +1,20 @@
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
+
+const initialSate = [{
+  name: 'apple',
+  quantity: 0,
+  price: 0.39
+}, {
+  name: 'banana',
+  quantity: 0,
+  price: 0.79
+}, {
+  name: 'cherry tomatoes',
+  quantity: 0,
+  price: 3.99
+}
+
+];
 
 const items = [{
   name: 'apple',
@@ -11,9 +27,38 @@ const items = [{
   price: 3.99
 }]
 
+const cartReducer = (state, action) => {
+  switch(action.type) {
+    case 'ADD_TO_CART':
+    const result = state.map((item) => {
+      if(item.name === action.payload.name) {
+        return {...item, quantity: item.quantity+1}
+      }
+      return item;
+    })
+    return result;
+    case 'ADD_ITEM_QUANTITY':
+    return state.map((item) => {
+      if(item.name === action.payload.name) {
+        return {...item, quantity: item.quantity+1}
+      }
+      return item;
+    })
+    case 'REMOVE_ITEM_QUANTITY':
+    return state.map((item) => {
+      if(item.name === action.payload.name & item.quantity !== 0) {
+        return {...item, quantity: item.quantity-1}
+      }
+      return item;
+    })
+    default:
+      throw new Error();
+  }
+}
 function ShoppingCart () {
   const cart = [{ name: 'apple', quantity: 3, price: 0.39 }]
-
+  const [cartState, dispatch] = useReducer(cartReducer, initialSate);
+  let totalAmount = 0.0;
   return (
     <div>
       <h1>Shopping Cart</h1>
@@ -24,30 +69,40 @@ function ShoppingCart () {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>${item.price}</p>
-              <button>Add to Cart</button>
+              <button onClick={
+                () => dispatch({type:'ADD_TO_CART', payload: {name: item.name}
+              })}>
+                Add to Cart
+              </button>
             </div>)
           )}
         </div>
         <div>
           <h2>Cart</h2>
-          {cart.map(item => (
-            <div key={item.name}>
+          {cartState.map(item => {
+          {totalAmount += item.quantity * item.price}
+          return (
+            (item.quantity > 0 ? <div key={item.name}>
               <h3>{item.name}</h3>
               <p>
-                <button>-</button>
+              <button onClick={() => dispatch({type: 'REMOVE_ITEM_QUANTITY', payload: {name: item.name}})}>-</button>
                 {item.quantity}
-                <button>+</button>
+                <button onClick={() => dispatch({type: 'ADD_ITEM_QUANTITY', payload: {name: item.name}})}>+</button>
               </p>
-              <p>Subtotal: ${item.quantity * item.price}</p>
+              <p>Subtotal: ${(item.quantity * item.price).toFixed(2)}</p>
+              
             </div>
-          ))}
+          : ''))})}
         </div>
       </div>
       <div className='total'>
-        <h2>Total: $0.00</h2>
+        <h2>Total: ${totalAmount.toFixed(2)}</h2>
       </div>
     </div>
   )
 }
 
 export default ShoppingCart
+
+
+// when click on add to cart add item, price and quantity to cart state
